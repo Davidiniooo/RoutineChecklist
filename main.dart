dd37ab4ddd0ecf8226063df44c6a45cb95981476
Davidiniooo <Davidinioooo@gmail.com>
2020-03-29 19:24:44 +0200

    Restructured classes

--- main.dart
+++ main.dart
@@ -4,15 +4,19 @@ void main(){
   runApp(MainApp());
 }
 
+class MainApp extends StatefulWidget{
+  @override
+  _MainAppState createState() => _MainAppState();
+}
 
-class MainApp extends StatelessWidget{
+class _MainAppState extends State<MainApp>{
   @override
   Widget build(BuildContext context){
     return MaterialApp
     (
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
+          backgroundColor: Colors.grey,
           title: Container(
             padding: EdgeInsets.all(5.0),
             child: Text("Routines"),
@@ -27,7 +31,7 @@ class MainApp extends StatelessWidget{
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          backgroundColor: Colors.deepOrange,
+          backgroundColor: Colors.grey,
         ),
         body: ListedRoutine(),
       ),
@@ -38,22 +42,26 @@ class MainApp extends StatelessWidget{
 class ListedRoutine extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.lightbulb_outline),
-              title: Text(
-                'Routine1'
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.arrow_forward,color: Colors.blue,)
-              ),
-            ),
-          ]
-        ),
-      ]
+    return ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context,i){
+        return SingeListedRoutine();
+      }
+    );
+  }
+}
+
+class SingeListedRoutine extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(Icons.lightbulb_outline,color: Colors.orange),
+      title: Text(
+          'Routine1'
+      ),
+      trailing: IconButton(
+          icon: Icon(Icons.arrow_forward,color: Colors.orange)
+      ),
     );
   }
 }
